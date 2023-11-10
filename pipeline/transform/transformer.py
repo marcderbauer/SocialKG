0aeba0c3a89e2c8acb5446ba0dc64a43b407d897
marcderbauer <marcanthony.bauer@gmail.com>
2023-11-10 12:03:58 +0100

    latest model, force json, add seed

--- pipeline/transform/transformer.py
+++ pipeline/transform/transformer.py
@@ -3,9 +3,10 @@ from pipeline.transform.prompts import prompts # ! Where to move?
 from pipeline.transform.llm import query_llm
 
 # TODO: Move into config file
-MODEL_NAME = "gpt-3.5-turbo"
-TEMPERATURE = 0
+MODEL_NAME = "gpt-3.5-turbo-1106"
+TEMPERATURE = 0.1
 TOP_P = 0.8
+SEED = 1234
 
 class Transformer():
     def __init__(self) -> None:
@@ -17,7 +18,7 @@ class Transformer():
         for prompt in prompts: # ! not immediately visible where prompts comes from, move into config
             llm_query = convert_prompt_to_llm_query(data, prompt)
     
-            response = query_llm(llm_query, model_name=MODEL_NAME, temperature=TEMPERATURE, top_p=TOP_P)
+            response = query_llm(llm_query, model_name=MODEL_NAME, temperature=TEMPERATURE, top_p=TOP_P, seed = SEED)
 
             result = process_response(response)
             results["entities"] += result.get("entities")
